f931693c6b81b03ac3930236710e701444b7cbb4
Sunny Toor <sunny@sunny.com>
2017-03-08 23:23:08 +1100

    complete gradient descent

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -7,27 +7,20 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
-for iter = 1:num_iters
+for iter = 1:num_iters,
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
+    tmpTheta = theta;
+    for j = 1:length(theta),
+	derivative = 0;
+	for i = 1:m,
+	    derivative = derivative + (X(i,:)*theta - y(i))*X(i,j);
+	end;
+	tmpTheta(j) = tmpTheta(j) - alpha*(derivative/m);
+    end;
+    theta = tmpTheta; 
 
-
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
-
-end
+    
+end;
 
 end
