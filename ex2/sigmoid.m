6851f58ca9a3ee3db74a4e4f66b8c5f6e3c4363b
Sunny Toor <sunny@sunny.com>
2017-07-24 21:32:03 +1000

    implement sigmoid function

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -9,7 +9,13 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+[rows, cols] = size(z);
 
+for i = 1:rows,
+    for j = 1:cols,
+	g(i,j) = 1/(1+e^-z(i,j));
+    end;
+end;
 
 
 
